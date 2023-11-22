23d5dc8277006e0e192453dc4ba229bfd086f71c
soybean15 <marlonpadilla1593@gmail.com>
2023-11-22 16:29:03 +0800

    added loading image

--- src/components/LoadingView.vue
+++ src/components/LoadingView.vue
@@ -2,8 +2,8 @@
     <Teleport to=".main">
   <div class="h-screen ">
     <div class="h-[700px] flex justify-center items-center"> 
-        <div class="flex flex-col items-center">
-            <img src="@/assets/loading.png"/>
+        <div class="flex flex-col justify-center items-center">
+            <img class="w-[200px]" src="@/assets/loading.png"/>
          
             <span>Sandale </span>
             <span class="loading loading-ring loading-lg"></span>
